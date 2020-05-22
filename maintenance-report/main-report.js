8523c0f2eaf5fa2d7cfe7ec0dfd14fd5157435e7
nicoleisbusy <60363959+nicoleisbusy@users.noreply.github.com>
2020-05-22 12:25:52 +0200

    maintenance report

--- maintenance-report/main-report.js
+++ maintenance-report/main-report.js
@@ -1,5 +1,5 @@
 let rootArray = [
-  ["Artemis", "Andy Weir", "Crown Publishing", "9780553448122", "305", "3.66/5", "5/5"],
+  ["<span onclick="showMa21()">Machine 21</span>", "Andy Weir", "Crown Publishing", "9780553448122", "305", "3.66/5", "5/5"],
   ["Annna Karenina", "Leo Tolstoy", "Vintage", "9780198748847", "964", "4.05/5", "4/5"]
 ];
 const title = document.getElementById("replaceT");
